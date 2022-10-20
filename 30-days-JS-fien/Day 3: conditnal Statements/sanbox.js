6ff941cdd32d25b9fcbcaca881f1ea843509f0d0
Fien-Dora <fiendora5@gmail.com>
2022-10-20 23:59:08 +0100

    feat: Conditional statement

---
+++ 30-days-JS-fien/Day 3: conditnal Statements/sanbox.js
@@ -0,0 +1,41 @@
+// 'use strict';
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+
+// let inputString = '';
+// let currentLine = 0;
+
+// process.stdin.on('data', function(inputStdin) {
+//     inputString += inputStdin;
+// });
+
+// process.stdin.on('end', function() {
+//     inputString = inputString.split('\n');
+
+//     main();
+// });
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function main() {
+    // const N = parseInt(readLine().trim(), 10);
+    let n = readLine();
+if(n%2 === 1){
+    console.log("Weird");
+}
+else if(n%2 === 0 && n>=2 && n <=5){
+    console.log('Not Weird');
+}
+else if(n%2 === 0 && n>=6 && n <=20){
+    console.log('Weird');
+}
+else if(n>=2){
+    console.log('Not Weird');
+}
+}
+main()
+
